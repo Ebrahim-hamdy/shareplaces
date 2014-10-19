6f330e86b709b84270014d2fb2e4b656b09fb289
Hima Hamdy <hema_fci@yahoo.com>
2014-10-19 19:20:11 +0200

    Status Update

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,12 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  #provider :facebook, ENV['726375684109637'], ENV['377d74e012651e3343eb4a2ebd87f412'], scope:"email, user_tagged_places"
+  provider :facebook, '726375684109637', '377d74e012651e3343eb4a2ebd87f412', scope:"email, user_tagged_places"
+  
+  if Rails.env.development? 
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE 
+end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shareplaces::Application.config.secret_key_base = '6b5bda508a388fb2a48285e56e62a40b176e3042c62a324d458f21dedfcd821db5da3db210aa8df64e73205cc4010683602a6c4f1055c3aa862459f4e2e983f6'
